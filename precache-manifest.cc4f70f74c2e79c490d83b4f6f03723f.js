f781345aba37fc12366fc0d3871d072b3667cec3
KeWa <kewa@kewa.io>
2020-03-10 20:53:53 -0700

    Updates

---
+++ precache-manifest.cc4f70f74c2e79c490d83b4f6f03723f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7a6c15556d04e4ec0129f65e77d0809",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0f81c6e95a59a1de3740",
+    "url": "/monsters-rolodex/static/css/main.a3facc43.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f81c6e95a59a1de3740",
+    "url": "/monsters-rolodex/static/js/main.d4c9af6d.chunk.js"
+  },
+  {
+    "revision": "24034436bd08b53a5646",
+    "url": "/monsters-rolodex/static/js/runtime-main.6386504b.js"
+  }
+]);
